7816fe242b835975d200e62e2629f4ac6d8959f7
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-29 02:47:23 -0500

    update card.tsx

--- src/modules/proveedor/components/card/card.tsx
+++ src/modules/proveedor/components/card/card.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
@@ -6,16 +7,14 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import {
-  User,
+  EditIcon,
+  FileText,
   MapPin,
   Phone,
-  FileText,
   TrashIcon,
-  EditIcon,
+  User,
 } from 'lucide-react'
-import { Skeleton } from '@/components/ui/skeleton'
 import { Provider } from '../../types/types.proveedor'
-import { Button } from '@/components/ui/button'
 
 export function ProveedorCard({
   proveedor,
@@ -26,71 +25,80 @@ export function ProveedorCard({
   btnDisabled: boolean
   proveedor?: Provider
   providerDelete: (id: string) => void
-  providerEdit: (provider: Provider) => void
+  providerEdit: (provider: Provider['proveedor'][number]) => void
 }) {
   if (!proveedor) return null
   const { proveedor: data } = proveedor
 
   const handledDelete = (id: string) => providerDelete(id)
-  const handledEdit = (provider: Provider) => providerEdit(provider)
+  const handledEdit = (provider: Provider['proveedor'][number]) =>
+    providerEdit(provider)
   return (
-    <Card className="overflow-hidden">
-      <CardHeader className="bg-primary/5 pb-3">
-        <CardTitle className="text-lg">
-          Razón Social: {data.corporate_reason}
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="pt-6 space-y-4">
-        <div className="flex items-start gap-3">
-          <FileText className="h-5 w-5 text-primary mt-0.5" />
-          <div className=" flex gap-3 items-center">
-            <p className="font-medium text-sm">RUC: </p>
-            <p>{data.ruc}</p>
-          </div>
-        </div>
+    <>
+      {data.map((data) => (
+        <Card key={data.id} className="overflow-hidden">
+          <CardHeader className="bg-primary/5 pb-3">
+            <CardTitle className="text-lg">
+              Razón Social: {data.corporate_reason}
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="pt-6 space-y-4">
+            <div className="flex items-start gap-3">
+              <FileText className="h-5 w-5 text-primary mt-0.5" />
+              <div className=" flex gap-3 items-center">
+                <p className="font-medium text-sm">RUC: </p>
+                <p>{data.ruc}</p>
+              </div>
+            </div>
 
-        <div className="flex items-start gap-3">
-          <User className="h-5 w-5 text-primary mt-0.5" />
-          <div className=" flex gap-3 items-center">
-            <p className="font-medium text-sm">Representante Legal: </p>
-            <p>{data.legalRepresentative}</p>
-          </div>
-        </div>
+            <div className="flex items-start gap-3">
+              <User className="h-5 w-5 text-primary mt-0.5" />
+              <div className=" flex gap-3 items-center">
+                <p className="font-medium text-sm">Representante Legal: </p>
+                <p>{data.legalRepresentative}</p>
+              </div>
+            </div>
 
-        <div className="flex items-start gap-3">
-          <MapPin className="h-5 w-5 text-primary mt-0.5" />
-          <div className=" flex gap-3 items-center">
-            <p className="font-medium text-sm">Dirección: </p>
-            <p>{data.address}</p>
-          </div>
-        </div>
+            <div className="flex items-start gap-3">
+              <MapPin className="h-5 w-5 text-primary mt-0.5" />
+              <div className=" flex gap-3 items-center">
+                <p className="font-medium text-sm">Dirección: </p>
+                <p>{data.address}</p>
+              </div>
+            </div>
 
-        <div className="flex items-start gap-3">
-          <Phone className="h-5 w-5 text-primary mt-0.5" />
-          <div className=" flex gap-3 items-center">
-            <p className="font-medium text-sm">Teléfono: </p>
-            <p>{data.phone}</p>
-          </div>
-        </div>
-      </CardContent>
-      {data.id && (
-        <CardFooter className="flex gap-3">
-          <Button
-            disabled={btnDisabled}
-            onClick={() => handledDelete(data.id)}
-            size={'icon'}
-          >
-            <TrashIcon />
-          </Button>
-          <Button
-            disabled={btnDisabled}
-            onClick={() => handledEdit(proveedor)}
-            size={'icon'}
-          >
-            <EditIcon />
-          </Button>
-        </CardFooter>
-      )}
-    </Card>
+            <div className="flex items-start gap-3">
+              <Phone className="h-5 w-5 text-primary mt-0.5" />
+              <div className=" flex gap-3 items-center">
+                <p className="font-medium text-sm">Teléfono: </p>
+                <p>{data.phone}</p>
+              </div>
+            </div>
+          </CardContent>
+          {data.id && (
+            <CardFooter className="flex gap-3">
+              <Button
+                disabled={btnDisabled}
+                onClick={() => handledDelete(data.id)}
+                size={'icon'}
+              >
+                <TrashIcon />
+              </Button>
+              <Button
+                disabled={btnDisabled}
+                onClick={() =>
+                  handledEdit(
+                    proveedor.proveedor.find((p) => p.id === data.id)!
+                  )
+                }
+                size={'icon'}
+              >
+                <EditIcon />
+              </Button>
+            </CardFooter>
+          )}
+        </Card>
+      ))}
+    </>
   )
 }
